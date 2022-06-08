2db770e07787d353bac4c5f50a4e77d7a1f8727c
Mauricio Peredo <mauricio@debianps1.debianps1>
2022-06-08 12:03:29 -0400

    first commit

--- src/views/FormView.vue
+++ src/views/FormView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="blog">
+  <div class="form">
     <h1>This is an about page</h1>
   </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,25 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div class="">
+     <v-container grid-list-md>
+         <v-layout row wrap>
+             <v-flex xs0 xl6 lg6 md6 sm6>
+                 
+              <v-img src="@/assets/ic.png"></v-img>
+
+             </v-flex>
+               <v-flex xs0 xl6 lg6 md6 sm6>
+
+                   <h2>halo</h2>
+               </v-flex>
+         </v-layout>
+
+     </v-container>
+
+ </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+export default{
+  
 }
 </script>
